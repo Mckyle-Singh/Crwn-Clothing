c50d198455685fb20231d6a742de9962e8082a3e
User Name <email@domain.co.za>
2023-06-02 13:19:51 +0200

    -added styling to App -made categories more dynamic

--- src/App.js
+++ src/App.js
@@ -1,48 +1,43 @@
+import "./categories.styles.scss"
 
+const App = () => {
+  
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats'
+    },
+    {
+      id: 2,
+      title: 'Jackets'
+    },
+    {
+      id: 3,
+      title: 'Sneakers'
+    },
+    {
+      id: 4,
+      title: 'Womens'
+    },
+    {
+      id: 5,
+      title: 'Mens'
+    }
+  ]
 
-const App=()=> {
   return (
     <div className="categories-container">
-      <div className="category-container">
-        {/* <img></img> */}
+      
+      {categories.map(({ title,id }) => (
+        <div key={id} className="category-container">
+          <div className="backgrounf-image"/>
           <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+            <h2>{ title}</h2>
+            <p>Shop Now</p>
           </div>
-      </div>
-
-      <div className="category-container">
-        {/* <img></img> */}
-          <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-          </div>
-      </div>
-
-      <div className="category-container">
-        {/* <img></img> */}
-          <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-          </div>
-      </div>
-
-      <div className="category-container">
-        {/* <img></img> */}
-          <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-          </div>
-      </div>
-
-      <div className="category-container">
-        {/* <img></img> */}
-          <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-          </div>
-      </div>
-
+        </div>
+      ))}
+     
     </div>
   );
 }
